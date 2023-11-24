304c13561eecab96bc375506fc8e35f05c372939
arhip14 <kruvdyk1503@gmail.com>
2023-11-24 22:27:30 +0200

    commit-2

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,10 +8,8 @@ const {schemas} = require("../../models/user");
 
 const router = express.Router();
 
-// signup
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
-// signin
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 router.get("/current", authenticate, ctrl.getCurrent);
